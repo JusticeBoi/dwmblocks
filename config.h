d2dba0c8dd361297ef5cd86bb77e79fe2dc06e71
oguz <oguzoztoprak@gmail.com>
2020-04-16 22:53:05 +0200

    my config

--- config.h
+++ config.h
@@ -2,21 +2,11 @@
 static const Block blocks[] = {
 	/*Command*/	/*Update Interval*/	/*Update Signal*/
 	{"cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"music",	0,	11}, */
-	{"pacpackages",	0,	8},
-	/* {"crypto",	0,	13}, */
-	/* {"torrent",	20,	7}, */
-	{"news",	0,	6},
-	/* {"memory",	10,	14}, */
-	/* {"cpu",		10,	13}, */
-	/* {"moonphase",	18000,	5}, */
-	{"weather",	18000,	5},
-	{"mailbox",	180,	12},
-	{"volume",	0,	10},
-	{"battery",	5,	3},
-	{"clock",	60,	1},
-	{"internet",	5,	4},
-	{"help-icon",	0,	15},
+	{"sound",	0,	5},
+	{"cpuUsage",	1,	4},
+	{"cpuTemp",	1,	4},
+	{"memory",	1,	4},
+	{"clock",	60,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
