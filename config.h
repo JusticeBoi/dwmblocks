24695d73b0c14cc23271eeecb1e99323c5c50d28
Oguz <oguz.oztoprak@tum.de>
2020-06-03 16:36:13 +0200

    Block struct changes in config

--- config.h
+++ config.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Command*/	/*Update Interval*/	/*Update Signal*/
-	{"up_down_speed",	1,	11},
-	{"pacpackages",	0,	10},
-	{"sound",	0,	5},
-	{"cpuUsage",	1,	4},
-	{"cpuTemp",	1,	8},
-	{"memory",	1,	7},
-	{"clock",	60,	1},
-	{"internet",	5,	6},
-	{"disk",	15,	9},
+	{"","up_down_speed",	1,	11},
+	{"","pacpackages",	0,	10},
+	{"","sound",	0,	5},
+	{"","cpuUsage",	1,	4},
+	{"","cpuTemp",	1,	8},
+	{"","memory",	1,	7},
+	{"","clock",	60,	1},
+	{"","internet",	5,	6},
+	{"","disk",	15,	9},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
